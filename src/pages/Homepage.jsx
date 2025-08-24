d1596af842a833e5af73a8c39f3b6c8bcddaab61
Ahmad-009 <manj2825@gmail.com>
2025-08-24 16:53:14 +0500

    fixes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/NavBar';
+import Navbar from '../components/Navbar';
 import Card from '../components/Card';
 import styles from './Homepage.module.css';
 import { CiWallet } from "react-icons/ci";
