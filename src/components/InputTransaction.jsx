70e222905aac540b84b4ae45b018ce953421cea7
Ahmad-009 <manj2825@gmail.com>
2025-08-24 10:42:01 +0500

    final changes

--- src/components/InputTransaction.jsx
+++ src/components/InputTransaction.jsx
@@ -1,13 +1,12 @@
 import styles from './InputTransaction.module.css';
 
-function InputTransaction({ title, Icon, active, onClick }) {
+function InputTransaction({ title, active, onClick }) {
   return (
     <div
       className={`${styles.container} ${active ? styles.active : styles.inactive}`}
       onClick={onClick}
     >
       <h2 className={styles.title}>
-        <Icon className={styles.Icon} />
         {title}
       </h2>
     </div>
